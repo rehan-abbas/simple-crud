4d41fa6395490a3ba2ce55d92bc55a3d62305557
rehan-abbas <139239998+rehan-abbas@users.noreply.github.com>
2024-09-20 23:21:15 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
-import CreatePost from "./components/createPost";
-import AllPosts from "./components/allPosts";
-import EditPost from "./components/editPost";
+import CreatePost from "./components/CreatePost";
+import AllPosts from "./components/AllPosts";
+import EditPost from "./components/EditPost";
 
 function App() {
   return (
